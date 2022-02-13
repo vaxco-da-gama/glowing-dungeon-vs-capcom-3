e7639ee6b9ae7365fabb689e25921a200dfac8c6
CharlesLB <charles.braga.jf.cb@gmail.com>
2022-02-13 18:27:39 -0300

    fix: target

--- src/main/java/Game/Application.java
+++ src/main/java/Game/Application.java
@@ -1,10 +1,14 @@
 package Game;
 
 import Game.Database.Database;
+import Game.View.SignUp;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Database.load();
+
+		SignUp signUp = new SignUp();
+		signUp.
 	}
 }
