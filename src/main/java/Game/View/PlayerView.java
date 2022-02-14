f4fd642fcc1fe9fbb9dafcd1285e4633d02dbe0c
CharlesLB <charles.braga.jf.cb@gmail.com>
2022-02-14 07:22:54 -0300

    feature: player page

--- src/main/java/Game/View/PlayerView.java
+++ src/main/java/Game/View/PlayerView.java
@@ -2,16 +2,33 @@ package Game.View;
 
 import Game.Config.Screen;
 import Game.Controllers.UserController;
+import Game.Controllers.ZoneController;
 import Game.Database.Database;
+import Game.Models.Domain.Zone;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlayerView extends JFrame {
 	private JPanel container;
 	private JPanel navbar;
 	private JButton sairButton;
+	private JButton criarPersonagemButton;
+	private JButton escolherZonaButton;
+	private JButton escolherPersonagemButton;
+	private JScrollPane Personagens;
+	private JTable personagensTable;
+	private JScrollPane zonas;
+	private JTable zonasTable;
+	private JButton salvarButton;
+
+	private List<Zone> zones = new ArrayList<>();
+	private List<Character> characters = new ArrayList<>();
 
 	public void render() {
 		setContentPane(container);
@@ -30,6 +47,23 @@ public class PlayerView extends JFrame {
 				signOut();
 			}
 		});
+	}
+
+	private void fillTableZoneTable() {
+		String[] columns = { "Nome", "Descrição" };
+		DefaultTableModel tableModel = (DefaultTableModel) zonasTable.getModel();
+
+		tableModel.setColumnIdentifiers(columns);
+		zonasTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		zones = ZoneController.findAll();
+
+		for (Zone zone : zones) {
+			tableModel.addRow(new Object[] {
+					zone.getName(),
+					zone.getDescription()
+			});
+		}
 	}
 
 	private void signOut() {
--- src/main/java/Game/View/SelectZoneView.java
+++ src/main/java/Game/View/SelectZoneView.java
@@ -1,38 +1,78 @@
 package Game.View;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 import Game.Config.Screen;
+import Game.Controllers.ZoneController;
+import Game.Models.Domain.Zone;
+import Game.Utils.Session;
 
 import java.awt.*;
+import java.util.List;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SelectZoneView extends JFrame {
 	private JPanel container;
 	private JTable table;
 	private JButton selecionarButton;
 
-	public SelectZoneView() throws HeadlessException {
-		setupListeners();
-		render();
-	}
+	private List<Zone> zones;
 
 	private void render() {
 		setContentPane(container);
-		setTitle("Glowing Dungeon VS Capcom | Products");
+		setTitle("Selecionar Zona");
 		setSize(Screen.getWidth(), Screen.getHeight());
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setVisible(true);
+
+		fillTableZoneTable();
+		setupListeners();
 	}
 
 	private void setupListeners() {
+		selecionarButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int selectedRow = table.getSelectedRow();
 
+				if (table.getSelectedRowCount() == 0) {
+					JOptionPane.showMessageDialog(null, "Selecione uma zona para continuar");
+					return;
+				}
+
+				if (table.getSelectedRowCount() > 0) {
+					JOptionPane.showMessageDialog(null, "Selecione somente uma zona para continuar");
+					return;
+				}
+
+				Zone zone = zones.get(selectedRow);
+				Session.setZone(zone);
+				// dispose();
+			}
+		});
 	}
 
-	private void setupContent() {
+	private void fillTableZoneTable() {
+		String[] columns = { "Nome", "Descrição" };
+		DefaultTableModel tableModel = getTableModel();
 
+		tableModel.setColumnIdentifiers(columns);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		zones = ZoneController.findAll();
+
+		for (Zone zone : zones) {
+			tableModel.addRow(new Object[] {
+					zone.getName(),
+					zone.getDescription()
+			});
+		}
 	}
 
-	private void setupTable() {
-		String[] columns = { "Name", "Value", "Quantity", "Description" };
+	private DefaultTableModel getTableModel() {
+		return (DefaultTableModel) table.getModel();
 	}
 }
